c7b5f9be8fb66cb703efbb561f71e874b48295d9
ferrigod <72757700+ferrigod@users.noreply.github.com>
2020-10-13 15:28:15 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,3 +1,6 @@
 # Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
+def my_email:
+  correo=print('fernando.amo@alu.uclm.es')
+  return correo
 
